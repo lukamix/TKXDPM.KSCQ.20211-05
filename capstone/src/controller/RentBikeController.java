f1906b4e2bc59419e17a4d85e30479b1c96f0d25
Duc <62656512+lukamix@users.noreply.github.com>
2022-01-04 23:57:27 +0700

    add some javadoc

--- capstone/src/controller/RentBikeController.java
+++ capstone/src/controller/RentBikeController.java
@@ -6,27 +6,43 @@ import common.exception.BikeNotAvailableException;
 import entity.parking.Parking;
 
 public class RentBikeController {
-/**
- * controller rentbike
- * 
- */
+
+	/**
+	 * @param bikeID
+	 */
 	public void requestToRentBike(String bikeID) {
-		/**
-		 * request to rent bike
-		 * @param bikeID
-		 */
+		
 	}
+	
+	/**
+	 * This method request to payment.
+	 */
 	public void requestPayment() {
 		
 	}
+	/**
+	 * This method make a transaction to deposit money for rent bike
+	 * @param bike_cost is value of the bike
+	 * @return deposit fee
+	 */
 	public int depositMoney(int bike_cost) {
 		int fee = 0;
 		fee = (int) (bike_cost*0.4);
 		return fee;
 	}
+	/**
+	 * This method check Availability of the bike
+	 * @param bikeID is the id of the bike
+	 * @param idparking the id of parking place
+	 * @throws SQLException check exception while connect to database
+	 * @throws BikeNotAvailableException check exception if bike is not available.
+	 */
 	public void checkBikeAvailability(int bikeID, int idparking) throws SQLException, BikeNotAvailableException{
        Parking.checkBikeAvailability(bikeID, idparking);
     }
+	/**
+	 * This method confirm payment transaction
+	 */
 	public void confirmPayment() {
 		
 	}
